b7e4b052d4c1396573736217586292e1b8a3ffb7
Sohae Park <ipnsohe@naver.com>
2025-02-14 00:54:29 +0900

    엔티티 생성

--- src/main/java/org/study/shopping/Item.java
+++ src/main/java/org/study/shopping/Item.java
@@ -1,4 +1,17 @@
 package org.study.shopping;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Item {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    @Column(nullable = false)
+    public String title;
+
+    @Column(nullable = false)
+    public Integer price;
 }
+
--- src/main/java/org/study/shopping/Notice.java
+++ src/main/java/org/study/shopping/Notice.java
@@ -1,4 +1,19 @@
 package org.study.shopping;
 
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
 public class Notice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String date;
 }
