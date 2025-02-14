aa9ce7d86ad9cecd414693c742e21fb987a0ca47
Sohae Park <ipnsohe@naver.com>
2025-02-14 10:52:03 +0900

    DB 데이터 출력

--- src/main/java/org/study/shopping/ItemController.java
+++ src/main/java/org/study/shopping/ItemController.java
@@ -1,14 +1,21 @@
 package org.study.shopping;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class ItemController {
+
+    private final ItemRepository itemRepository;
 
     @GetMapping("/list")
     String list(Model model){
+        List<Item> result = itemRepository.findAll();
         model.addAttribute("전달할데이터이름", "치마");
         return "list.html";
     }
